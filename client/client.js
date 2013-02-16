1e1cff4a6465f48e7ef424d9f6ef999b2d658f1f
Oleg Trofimov <otrofimov@twitter.com>
2013-02-16 15:29:28 -0800

    ID3 tag library

--- client/client.js
+++ client/client.js
@@ -17,7 +17,7 @@ function openFilePicker (){
       services:['COMPUTER', 'DROPBOX', 'GMAIL', 'GOOGLE_DRIVE', 'URL'],
     },
     function(FPFile){
-      console.log(JSON.stringify(FPFile));
+      var file = FPFile
     },
     function(FPError){
       console.log(FPError.toString());
@@ -25,3 +25,17 @@ function openFilePicker (){
   );
 }
 
+// URL of the mp3 file (must be on the same domain!)
+// var file = "something.mp3";
+// define your own callback function
+function mycallback() {
+ // either call the ID3.getAllTags([file]) function which returns an object holding all the tags
+ alert(
+  "All tags in this file: " + ID3.getAllTags(file).toSource()
+ );
+ // or call ID3.getTag([file], [tag]) to get a specific tag
+ alert(
+  "Title: " + ID3.getTag(file, "title") + " by artist: " + ID3.getTag(file, "artist")
+ );
+}
+ID3.loadTags(file, mycallback);
