5caedfa2234ce636b38537ab509253ac51d29935
Emma Lucia <emmalucia@Emmas-MacBook-Air.local>
2013-02-16 23:03:48 -0800

    fix array buffer so that mp3 gets parsed no matter what the id3 version

--- client/client.js
+++ client/client.js
@@ -13,37 +13,36 @@ Template.playlist.song = function () {
 
 Template.upload_file.events({
 															'change input':  function (e) {
-																var m = +(new Date);
 																var obj;
-
 																filepicker.setKey("AgsF6GExRRJejABwf1FSpz");
 																filepicker.store(e.target,
 									function(FPFile){
-										console.log (Date.now() - m)
 										console.log("Store successful:", JSON.stringify(FPFile));
 										Playlist.insert(_.extend(FPFile, obj))
 									},
 									function(FPError) { console.log(FPError.toString()) },
-									function(progress) { console.log("Loading: "+progress+"%") })
-var reader = new FileReader;
- reader.onload = function(e) {
-    var dv = new jDataView(this.result);
-
-    // "TAG" starts at byte -128 from EOF.
-    // See http://en.wikipedia.org/wiki/ID3
-    if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
-			obj = {
-				title : dv.getString(30, dv.tell()),
-				artist : dv.getString(30, dv.tell()),
-				album : dv.getString(30, dv.tell()),
-				year : dv.getString(4, dv.tell())
-			}
-			console.log(obj)
-    } else {
-      // no ID3v1 data found.
-    }
-  };
-  reader.readAsArrayBuffer(e.target.files[0]);
+									function(progress) { "Loading: "+progress+"%" })
+																var reader = new FileReader;
+																reader.onload = function(e) {
+																	var dv = new jDataView(this.result);
+																	if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
+																		obj = {
+																			title : dv.getString(30, dv.tell()),
+																			artist : dv.getString(30, dv.tell()),
+																			album : dv.getString(30, dv.tell()),
+																			year : dv.getString(4, dv.tell())
+																		}
+																	}
+																	else {
+																		var k = dv.getString(dv.byteLength * .1 ,0)
+																		var bf = new BinaryFile(k);
+																		var reader = getTagReader(bf)
+																		reader.loadData(bf, function () {
+																											obj = reader.readTagsFromData(bf)
+																										})
+																	}
+																}
+																reader.readAsArrayBuffer(e.target.files[0]);
 															}
 														})
 
